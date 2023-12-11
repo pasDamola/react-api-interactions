9fb5e8eb0dc22ab13427e318731d1286f8860067
pasDamola <oyindadavid18@gmail.com>
2023-12-11 14:14:54 +0100

    fetch data from rest api using React's useEffect

---
+++ api-interactions-useEffect/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { PostsPage } from "./posts/PostsPage";
+
+function App() {
+  return <PostsPage />;
+}
+
+export default App;
